51c89e8d9808f3079a03e204a4379427c4a8cc57
João Brito <joaogbrito@gmail.com>
2021-09-20 14:15:33 +0100

    store in file gob

--- store.go
+++ store.go
@@ -1,18 +1,40 @@
 package main
 
 import (
+	"encoding/gob"
+	"io"
+	"log"
+	"os"
 	"sync"
 )
 
-type URLStore struct {
-	urls map[string]string
-	mu sync.RWMutex
+type record struct {
+	Key, URL string
 }
 
-func NewURLStore() *URLStore{
-	return &URLStore{
+type URLStore struct {
+	urls map[string]string
+	mu   sync.RWMutex
+	file *os.File
+}
+
+func NewURLStore(filename string) *URLStore {
+	store := &URLStore{
 		urls: make(map[string]string),
 	}
+
+	f, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatal("URLStore: ", err)
+	}
+
+	store.file = f
+
+	if err := store.load(); err != nil {
+		log.Println("Error loading data in URLStore: ", err)
+	}
+
+	return store
 }
 
 func (s *URLStore) Get(key string) string {
@@ -42,7 +64,37 @@ func (s *URLStore) Put(url string) string {
 	for {
 		key := genKey(s.Count()) // generate the short URL
 		if ok := s.Set(key, url); ok {
+			if err := store.save(key, url); err != nil {
+				log.Println("Error saving to URLStore: ", err)
+			}
 			return key
 		}
 	}
+}
+
+func (s *URLStore) save(key, url string) error {
+	e := gob.NewEncoder(s.file)
+	return e.Encode(record{key, url})
+}
+
+func (s *URLStore) load() error {
+	if _, err := s.file.Seek(0, 0); err != nil {
+		return err
+	}
+
+	d := gob.NewDecoder(s.file)
+
+	var err error
+	for err == nil {
+		var r record
+		if err = d.Decode(&r); err == nil {
+			s.Set(r.Key, r.URL)
+		}
+	}
+
+	if err == io.EOF {
+		return nil
+	}
+
+	return err
 }
